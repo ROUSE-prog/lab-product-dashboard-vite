211ec8f96a511dbc250d5f68a430427e7f5d0f2a
ROUSE-prog <stevenr2049@gmail.com>
2026-05-07 14:25:08 -0400

    retry for test!

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
-import ProductList from './components/ProductList';
+import React, { useState } from 'react'
+import ProductList from './components/ProductList'
+import './App.css'
 
-const App = () => {
-  // TODO: Define initial product data
+const initialProducts = [
+  { id: 1, name: 'Laptop', price: '$999', inStock: true },
+  { id: 2, name: 'Phone', price: '$699', inStock: false },
+  { id: 3, name: 'Tablet', price: '$499', inStock: true },
+]
 
-  // TODO: Implement state to manage filtering
+function App() {
+  const [products, setProducts] = useState(initialProducts)
+  const [showInStockOnly, setShowInStockOnly] = useState(false)
 
-  // TODO: Implement logic to filter products based on availability
+  const displayedProducts = showInStockOnly
+    ? products.filter((product) => product.inStock)
+    : products
+
+  const removeProduct = (id) => {
+    setProducts(products.filter((product) => product.id !== id))
+  }
 
   return (
-    <div>
-      <h1>{/* TODO: Add 'Product Dashboard' title here */}</h1>
-      
-      {/* TODO: Add buttons to allow filtering by availability */}
+    <main className="app">
+      <h1>Product Dashboard</h1>
 
-      {/* TODO: Render the ProductList component and pass filtered products */}
-      
-    </div>
-  );
-};
+      <button onClick={() => setShowInStockOnly(!showInStockOnly)}>
+        {showInStockOnly ? 'Show All Products' : 'Show In Stock Only'}
+      </button>
 
-export default App;
+      <ProductList products={displayedProducts} onRemove={removeProduct} />
+    </main>
+  )
+}
+
+export default App
